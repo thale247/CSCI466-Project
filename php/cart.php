41e09276d98201acf08f0163947765612acc41a3
Leyla Kucukkaya <lkucukkaya826@gmail.com>
2025-04-24 21:50:29 -0500

    Create cart.php
    Temporary skeleton

---
+++ php/cart.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Viewing Cart</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="nav navbar-dark bg-dark d-flex align-items-center" style="height:75px;">
+        <a class="navbar-brand" href="#" style="padding:20px;">Online Parts Store</a>
+        <a class="nav-link text-secondary" href="/index.php" style="padding:20px;">Shop For Parts</a>
+        <a class="nav-link text-secondary" href="#" style="padding:20px;">Inventory</a>
+        <div class="ml-auto">
+            <a class="nav-link active" href="#" style="padding:20px;color:blue;">View Cart</a>
+        </div>
+    </nav>
+</body>
+
+</html>
