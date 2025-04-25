0083e1e329f6b39fde62f2cbc70b3b7daa265416
thale247 <tomhale247@gmail.com>
2025-04-25 16:42:48 -0500

    Create db_connect.php

---
+++ php/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'courses';
+$dbname = ''; // FILL THESE IN FOR YOUR SERVER
+$username = '';
+$password = ''; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
